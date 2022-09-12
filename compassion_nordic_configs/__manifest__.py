960da4a54f2928379be3b0a03491be6a38879d73
david <dwulliamoz@compassion.ch>
2022-09-12 10:57:35 +0200

    adding nordic config module

---
+++ compassion_nordic_configs/__manifest__.py
@@ -0,0 +1,15 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# pylint: disable=C8101
+{
+    'name': 'Compassion Norden base config',
+    'version': '14.0.0.0.0',
+    'license': 'AGPL-3',
+    'author': 'CompassionCH',
+    'website': 'http://www.compassion.ch',
+    'category': 'Localization',
+    'data': [
+        'data/norden_product.xml',
+        'data/res.users.csv',
+
+    ],
+}
