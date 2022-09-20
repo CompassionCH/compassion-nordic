80f1a66f0d2521384988c32103fba647daf3551e
robin <robin.berguerand@gmail.com>
2022-09-20 12:39:00 +0200

    Update the account payment order to use the correct fields.

--- compassion_denmark_payment/models/account_payment_order.py
+++ compassion_denmark_payment/models/account_payment_order.py
@@ -21,18 +21,25 @@ class AccountPaymentOrder(models.Model):
         if self.payment_method_id.code != "denmark_direct_debit":
             return super().generate_payment_file()
 
-        data_delivery = beservice.DataDelivery(data_supplier_number=self.payment_mode_id.initiating_party_issuer,
+        data_delivery = beservice.DataDelivery(data_supplier_number=self.payment_mode_id.initiating_party_scheme,
                                                subsystem='BS1', delivery_identification=1)
+
         data_delivery.add_section(data_supplier_id=self.payment_mode_id.initiating_party_identifier,
-                                  pbs_number=self.company_partner_bank_id.acc_number,
+                                  pbs_number=self.payment_mode_id.initiating_party_issuer,
                                   debtor_group_number=1)
 
-        for payment_line in self.payment_line_ids:
-            val = payment_line.move_line_id.move_id.line_ids.mapped('contract_id')
-            data_delivery.sections[0].add_payment(customer_number=f'{payment_line.partner_id.ref:15}',
-                                                  mandate_number=payment_line.mandate_id.id,
-                                                  amount=payment_line.amount_currency,
+        for bank_line in self.bank_line_ids:
+            text_lines = []
+            for line in bank_line.payment_line_ids:
+                for invoice_line in line.move_line_id.move_id.invoice_line_ids:
+                    text_lines.append((invoice_line.product_id.id, f"{int(invoice_line.credit)}  {invoice_line.name}"))
+            text_lines.sort(key=lambda a: a[0])
+
+            data_delivery.sections[0].add_payment(customer_number=f'{bank_line.partner_id.ref:15}',
+                                                  mandate_number=bank_line.mandate_id.id,
+                                                  amount=bank_line.amount_currency,
                                                   sign_code=beservice.SignCode.COLLECTION,
-                                                  payment_date=payment_line.date,
+                                                  payment_date=bank_line.date,
+                                                  text_lines=text_lines
                                                   )
         return data_delivery.to_ocr().encode('iso-8859-1'), "{}.xml".format(self.name)
