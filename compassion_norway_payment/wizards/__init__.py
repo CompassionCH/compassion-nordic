f12ec7a9186ce4e185da05622de0aebd0dfece2e
robin <robin.berguerand@gmail.com>
2022-09-22 15:20:48 +0200

    Add Mandate Upload functionality

---
+++ compassion_norway_payment/wizards/__init__.py
@@ -0,0 +1,12 @@
+##############################################################################
+#
+#    Copyright (C) 2022 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Robin Berguerand (robin.berguerand@gmail.com)
+#
+
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+
+from . import load_mandate_wizard
