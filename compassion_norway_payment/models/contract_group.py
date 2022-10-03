41dc14113782e5bc884a1d1c0fa4cc593d20c950
robin <robin.berguerand@gmail.com>
2022-10-03 13:56:00 +0200

    Add automatic computation of kid

--- compassion_norway_payment/models/contract_group.py
+++ compassion_norway_payment/models/contract_group.py
@@ -8,13 +8,10 @@
 #
 ##############################################################################
 
-import logging
-from datetime import datetime, date
 
-from dateutil.relativedelta import relativedelta
+from functools import reduce
 
 from odoo import api, fields, models, _
-from odoo.tools import config
 
 
 class ContractGroup(models.Model):
@@ -25,3 +22,21 @@ class ContractGroup(models.Model):
     ##########################################################################
     notify_payee = fields.Boolean('Notify Payee',
                                   default=False, required=True)
+
+
+class RecurringContract(models.Model):
+    _inherit = "recurring.contract"
+
+    @api.model
+    def create(self, vals):
+        group = self.env['recurring.contract.group'].browse(vals.get('group_id'))
+        if group.payment_mode_id.payment_method_code == "norway_direct_debit":
+            if vals.get('reference', '/') == '/':
+                vals['reference'] = self.env['ir.sequence'].next_by_code(
+                    'recurring.contract.ref')
+            ref = f'7{vals["partner_id"]:05d}{int(vals["reference"][3:]):07d}'
+            check_digit = (10 - reduce(lambda a, b: (a + int(b / 10) + b),
+                                       map(lambda b: (2 if (b[0] & 1 == 0) else 1) * int(b[1]), enumerate(ref)),
+                                       0)) % 10
+            group.update({'ref': f"{ref}{check_digit}"})
+            return super().create(vals)
