4f07eefb8d2550eed2e6fb9fc3a357c931048158
robin <robin.berguerand@gmail.com>
2022-10-04 14:37:06 +0200

    Add missing view in manifest

--- compassion_nordic_accounting/__manifest__.py
+++ compassion_nordic_accounting/__manifest__.py
@@ -8,9 +8,11 @@
     "version": "14.0.1.3.3",
     "license": "AGPL-3",
     "category": "Banking addons",
-    "depends": ["account_banking_pain_base", "account_banking_mandate","recurring_contract","account_invoice_pricelist"],
+    "depends": ["account_banking_pain_base", "account_banking_mandate", "recurring_contract",
+                "account_invoice_pricelist"],
     "data": [
         "views/load_mandate_wizard_view.xml",
+        "views/mandate_update_view.xml",
         'security/ir.model.access.csv'
     ],
     "installable": True,
