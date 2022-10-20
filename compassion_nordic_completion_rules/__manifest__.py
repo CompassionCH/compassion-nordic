2998beb0627dc06eded2d29989007bc0d2a31d57
david <dwulliamoz@compassion.ch>
2022-10-20 18:02:37 +0200

    add some completion

---
+++ compassion_nordic_completion_rules/__manifest__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#       ______ Releasing children from poverty      _
+#      / ____/___  ____ ___  ____  ____ ___________(_)___  ____
+#     / /   / __ \/ __ `__ \/ __ \/ __ `/ ___/ ___/ / __ \/ __ \
+#    / /___/ /_/ / / / / / / /_/ / /_/ (__  |__  ) / /_/ / / / /
+#    \____/\____/_/ /_/ /_/ .___/\__,_/____/____/_/\____/_/ /_/
+#                        /_/
+#                            in Jesus' name
+#
+#    Copyright (C) 2016-2022 Compassion CH (http://www.compassion.ch)
+#    @author: Robin Berguerand <robin.berguerand@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': "compassion_nordic_completion_rules",
+
+    'summary': """
+        statement completion rules to add some automation",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Compassion CH",
+    'website': "https://www.compassion.ch",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['compassion_nordic_accounting'],
+
+    # always loaded
+    'data': [
+
+    ],
+    # only loaded in demonstration mode
+    #'demo': [
+    #    'demo/demo.xml',
+    #],
+}
