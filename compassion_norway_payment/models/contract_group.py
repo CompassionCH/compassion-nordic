ce6ab1a201934420ce82590755704468aac77d03
davidwul <dwulliamoz@compassion.ch>
2022-10-28 13:58:54 +0200

    Update contract_group.py
    cast ref as int

--- compassion_norway_payment/models/contract_group.py
+++ compassion_norway_payment/models/contract_group.py
@@ -35,7 +35,7 @@ class RecurringContract(models.Model):
                 vals['reference'] = self.env['ir.sequence'].next_by_code(
                     'recurring.contract.ref')
             partner_ref = self.env['res.partner'].browse(vals.get('partner_id')).ref
-            ref = f'7{partner_ref:05d}{int(vals["reference"][3:]):07d}'
+            ref = f'7{int(partner_ref):05d}{int(vals["reference"][3:]):07d}'
             check_digit = (10 - reduce(lambda a, b: (a + int(b / 10) + b),
                                        map(lambda b: (2 if (b[0] & 1 == 0) else 1) * int(b[1]), enumerate(ref)),
                                        0)) % 10
