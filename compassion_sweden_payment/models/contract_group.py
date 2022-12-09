2b8b59628d5e13388c0833e6f4e33e2b7d1d1008
Simon Gonzalez <simon.gonzalez@bluewin.ch>
2022-12-09 15:53:38 +0100

    cp-290 sweden payment option should always automatically generate a reference

--- compassion_sweden_payment/models/contract_group.py
+++ compassion_sweden_payment/models/contract_group.py
@@ -11,7 +11,8 @@
 
 
 from functools import reduce
-from odoo import models, api, fields
+
+from odoo import models, api
 
 
 class RecurringContract(models.Model):
@@ -20,14 +21,13 @@ class RecurringContract(models.Model):
     @api.model
     def create(self, vals):
         group = self.env['recurring.contract.group'].browse(vals.get('group_id'))
-        if group.payment_mode_id.payment_method_code == "sweden_direct_debit":
-            if vals.get('reference', '/') == '/':
-                vals['reference'] = self.env['ir.sequence'].next_by_code(
-                    'recurring.contract.ref')
-                partner_ref = self.env['res.partner'].browse(vals.get('partner_id')).ref
-                ref = f'7{int(partner_ref):05d}{int(vals["reference"][3:]):09d}'
-                check_digit = (10 - reduce(lambda a, b: (a + int(b / 10) + b),
-                                           map(lambda b: (2 if (b[0] & 1 == 0) else 1) * int(b[1]), enumerate(ref)),
-                                           0)) % 10
-                group.update({'ref': f"{ref}{check_digit}"})
+        if vals.get('reference', '/') == '/':
+            vals['reference'] = self.env['ir.sequence'].next_by_code(
+                'recurring.contract.ref')
+            partner_ref = self.env['res.partner'].browse(vals.get('partner_id')).ref
+            ref = f'7{int(partner_ref):05d}{int(vals["reference"][3:]):09d}'
+            check_digit = (10 - reduce(lambda a, b: (a + int(b / 10) + b),
+                                       map(lambda b: (2 if (b[0] & 1 == 0) else 1) * int(b[1]), enumerate(ref)),
+                                       0)) % 10
+            group.update({'ref': f"{ref}{check_digit}"})
         return super().create(vals)
