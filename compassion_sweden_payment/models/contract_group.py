89190bc32fb437b93b05f19447133f626e5d9fbc
Simon Gonzalez <simon.gonzalez@bluewin.ch>
2022-12-13 10:42:32 +0100

    cp-290 danish and norvegian shouldn't be impacted by this

--- compassion_sweden_payment/models/contract_group.py
+++ compassion_sweden_payment/models/contract_group.py
@@ -20,14 +20,15 @@ class RecurringContract(models.Model):
 
     @api.model
     def create(self, vals):
-        group = self.env['recurring.contract.group'].browse(vals.get('group_id'))
-        if vals.get('reference', '/') == '/':
-            vals['reference'] = self.env['ir.sequence'].next_by_code(
-                'recurring.contract.ref')
-            partner_ref = self.env['res.partner'].browse(vals.get('partner_id')).ref
-            ref = f'7{int(partner_ref):05d}{int(vals["reference"][3:]):09d}'
-            check_digit = (10 - reduce(lambda a, b: (a + int(b / 10) + b),
-                                       map(lambda b: (2 if (b[0] & 1 == 0) else 1) * int(b[1]), enumerate(ref)),
-                                       0)) % 10
-            group.update({'ref': f"{ref}{check_digit}"})
+        if self.env.company.country_id == self.env.ref('base.se'):
+            group = self.env['recurring.contract.group'].browse(vals.get('group_id'))
+            if vals.get('reference', '/') == '/':
+                vals['reference'] = self.env['ir.sequence'].next_by_code(
+                    'recurring.contract.ref')
+                partner_ref = self.env['res.partner'].browse(vals.get('partner_id')).ref
+                ref = f'7{int(partner_ref):05d}{int(vals["reference"][3:]):09d}'
+                check_digit = (10 - reduce(lambda a, b: (a + int(b / 10) + b),
+                                           map(lambda b: (2 if (b[0] & 1 == 0) else 1) * int(b[1]), enumerate(ref)),
+                                           0)) % 10
+                group.update({'ref': f"{ref}{check_digit}"})
         return super().create(vals)
