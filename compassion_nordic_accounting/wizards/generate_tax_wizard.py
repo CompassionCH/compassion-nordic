43c8658d512ceb9d4e46321a3628ea5be983765a
Simon Gonzalez <simon.gonzalez@bluewin.ch>
2022-12-15 15:06:00 +0100

    CP-300 Adapt tax file generation and wizard

--- compassion_nordic_accounting/wizards/generate_tax_wizard.py
+++ compassion_nordic_accounting/wizards/generate_tax_wizard.py
@@ -7,23 +7,41 @@
 #    The licence is in the file __manifest__.py
 #
 ##############################################################################
-import base64
-import csv
-from functools import reduce
-from itertools import groupby
-from odoo import api, models, fields
-import io
 from datetime import datetime
-import xml.etree.ElementTree as ET
-from xml.dom import minidom
+
+from odoo import api, models, fields
+from odoo.exceptions import UserError
 
 
-class GenerateTaxWizard(models.Model):
+class GenerateTaxWizard(models.TransientModel):
     _name = "generate.tax.wizard"
     _description = "Generate Tax files"
 
-    year = fields.Char("Fiscal Year", default=str(datetime.now().year - 1))
+    tax_year = fields.Selection("_year_selection",
+                                "Calendar Year",
+                                default=str(datetime.today().year - 1),
+                                required=True
+                                )
     xml_version = fields.Float("XML version", default=8)
+    is_sweden = fields.Boolean(default=lambda s: s.env.company.country_id == s.env.ref("base.se"))
+
+    _sql_constraints = [
+        ("tax_year_not_in_the_future",
+         "CHECK(tax_year < EXTRACT(year FROM CURRENT_DATE))",
+         "Year of taxation can't be in the future"),
+    ]
+
+    @api.model
+    def _year_selection(self):
+        curr_year = datetime.today().year
+        year_list = []
+        while curr_year >= datetime.today().year - 15:
+            year_list.append((str(curr_year), str(curr_year)))
+            curr_year -= 1
+        return year_list
 
     def generate_tax(self):
-        pass
+        try:
+            raise NotImplementedError()
+        except NotImplementedError:
+            raise UserError("The company that you are on doesn't support this feature.")
