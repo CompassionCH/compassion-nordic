b0b2ea852132632272b3ad3461789d7317c2f260
Simon Gonzalez <simon.gonzalez@bluewin.ch>
2023-02-21 08:56:02 +0100

    CP-382 : Avoid the creation of the payment line as we don't use it in compassion processes.

---
+++ compassion_nordic_accounting/models/account_payment_order.py
@@ -0,0 +1,14 @@
+from odoo import models, fields
+
+
+class AccountPaymentOrder(models.Model):
+    _inherit = 'account.payment.order'
+
+    skip_payment_line = fields.Boolean(string="Skip payment lines",
+                                       help="Should the payment lines be generated ?",
+                                       default=True)
+
+    def draft2open(self):
+        ctx = self.env.context.copy()
+        ctx.update({"skip_payment_line": self.skip_payment_line})
+        return super(AccountPaymentOrder, self.with_context(ctx)).draft2open()
