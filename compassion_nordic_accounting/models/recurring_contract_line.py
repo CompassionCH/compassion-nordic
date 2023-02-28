31192bc936eb329e12eae3252171c7112f3c92a1
Emanuel Cino <ecino@compassion.ch>
2023-02-28 08:37:18 +0100

    [ADD] Cron for Nordic rate change process
    - One cron will remove the balance product from open invoices that will be generated before the rate change happens.
    - The second cron will remove the balance product from all sponsorships.
    - Those cron will be executed only once, but contain the logic for the transition to be smooth and correct.
    - TODO the correct dates should be entered in the database, one month before the rate change, when invoices are generated and then just after the rate change for the second CRON.
    - Remove also useless onchange method.

--- compassion_nordic_accounting/models/recurring_contract_line.py
+++ compassion_nordic_accounting/models/recurring_contract_line.py
@@ -1,16 +1,6 @@
-##############################################################################
-#
-#    Copyright (C) 2022 Compassion CH (http://www.compassion.ch)
-#    Releasing children from poverty
-#    @author: Robin Berguerand (robin.berguerand@gmail.com)
-#
-#    The licence is in the file __manifest__.py
-#
-##############################################################################
-
 import logging
 
-from odoo import api, fields, models
+from odoo import models
 
 _logger = logging.getLogger(__name__)
 
@@ -18,6 +8,8 @@ _logger = logging.getLogger(__name__)
 class ContractLine(models.Model):
     _inherit = "recurring.contract.line"
 
-    def recompute_price(self, pricelist_id):
-        data = pricelist_id.price_get(self.product_id.id, 1)
-        self.amount = data[pricelist_id.id]
+    def _remove_balance(self):
+        balance_product = self.env.ref("recurring_contract.product_balance_migr")
+        lines = self.search([("product_id", "=", balance_product.id)])
+        _logger.info("Removed %s balance lines", str(len(lines)))
+        return lines.unlink()
