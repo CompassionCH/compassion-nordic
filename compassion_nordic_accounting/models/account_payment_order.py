616f1752824e720dfe935194feb906e5cdfe2239
Simon <simon.gonzalez@bluewin.ch>
2023-03-29 14:54:39 +0200

    T0099 move the skip_payment_line on the payment mode The payment mode should own this functionality because in DK for example it's always the same behaviour.

--- compassion_nordic_accounting/models/account_payment_order.py
+++ compassion_nordic_accounting/models/account_payment_order.py
@@ -5,7 +5,9 @@ class AccountPaymentOrder(models.Model):
     _inherit = 'account.payment.order'
 
     skip_payment_line = fields.Boolean(string="Skip payment lines",
-                                       default=True)
+                                       help="This field is retrieved from the payment mode associated with this payment order.",
+                                       related='payment_mode_id.skip_payment_line',
+                                       readonly=True)
 
     def draft2open(self):
         return super(AccountPaymentOrder, self.with_context(skip_payment_line=self.skip_payment_line)).draft2open()
