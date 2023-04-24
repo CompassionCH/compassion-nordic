aa16cee3f033c540ae592f3132697be540b9fb31
NoeBerdoz <berdoznoe@gmail.com>
2023-04-24 15:21:02 +0200

    FIX: Update hard coded nordic condition

--- child_nordic/models/ir_actions_report.py
+++ child_nordic/models/ir_actions_report.py
@@ -6,6 +6,6 @@ class Report(models.Model):
 
     def _render_qweb_pdf(self, res_ids=None, data=None):
         # For unknown reason, res_ids are not passed when called by method report_action().
-        if "child_nordic" in self.report_name and not res_ids:
+        if "child_compassion" in self.report_name and not res_ids:
             res_ids = self.env.context.get("active_ids")
         return super()._render_qweb_pdf(res_ids, data)
