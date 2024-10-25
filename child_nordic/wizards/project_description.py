f399fca64503acbce3451fcb3b8d761c7b4cb487
robinrolle <robinrolle1748@gmail.com>
2024-10-25 10:02:18 +0200

    [MIG] T1877 child_nordic: import from 14.0

---
+++ child_nordic/wizards/project_description.py
@@ -0,0 +1,27 @@
+##############################################################################
+#
+#    Copyright (C) 2017 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    The licence is in the file __manifest__.py
+#
+##############################################################################
+from odoo import api, models
+
+
+class ChildDescription(models.TransientModel):
+    _inherit = "compassion.project.description"
+
+    NORDIC_DESCRIPTIONS = {
+        "sv_SE": "description_se",
+        "nb_NO": "description_no",
+        "da_DK": "description_da",
+        "fi_FI": "description_en",
+    }
+
+    @api.model
+    def _supported_languages(self):
+        res = super()._supported_languages()
+        res.update(self.NORDIC_DESCRIPTIONS)
+        return res
