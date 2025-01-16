3a76600885630c6749986d229e95c80fd4a6ee51
Emanuel Cino <ecino@compassion.ch>
2025-01-16 15:32:25 +0100

    FIX bug in report generation

--- child_nordic/models/ir_actions_report.py
+++ child_nordic/models/ir_actions_report.py
@@ -7,6 +7,6 @@ class Report(models.Model):
     def _render_qweb_pdf(self, report_ref, res_ids=None, data=None):
         # For unknown reason, res_ids are not passed
         # when called by method report_action().
-        if "child_compassion" in self.report_name and not res_ids:
+        if "child_compassion" in report_ref and not res_ids:
             res_ids = self.env.context.get("active_ids")
         return super()._render_qweb_pdf(report_ref, res_ids, data)
