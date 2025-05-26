199b8c3f0e84e26e77ee03ca0f49834dc63c3091
Emanuel Cino <ecino@compassion.ch>
2025-05-26 15:39:43 +0200

    T2399 [FIX] missing country_id on contracts

---
+++ compassion_nordic_accounting/migrations/17.0.1.0.2/pre-migration.py
@@ -0,0 +1,13 @@
+def migrate(cr, version):
+    """
+    Update the country_id field in recurring_contract based on partner_id.
+    This is necessary to ensure that all recurring contracts have a valid country_id
+    set, especially for those that were created before the country_id field was added.
+    """
+    cr.execute(
+        """
+        update recurring_contract set country_id = (
+            select p.country_id from res_partner p where p.id = partner_id
+        ) where country_id is null;
+        """
+    )
