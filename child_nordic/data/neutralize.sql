e1cd6aa3b2ac78d3c49346fd86618b1b04736e69
Emanuel Cino <ecino@compassion.ch>
2025-09-01 10:17:19 +0200

    ADD Neutralize script for odoo.sh (in child_nordic module)

---
+++ child_nordic/data/neutralize.sql
@@ -0,0 +1,41 @@
+-- Neutralize Script for Compassion Nordic database
+-- ruff: noqa: E501
+
+-- Children on hold
+delete from correspondence where child_id IN (select id from compassion_child where state in ('N','I'));
+delete from sponsorship_gift where child_id IN (select id from compassion_child where state in ('N','I'));
+delete from recurring_contract where child_id IN (select id from compassion_child where state in ('N','I'));
+delete from compassion_child where state in ('N','I');
+delete from compassion_hold where state = 'active';
+
+-- Changing parameters
+update ir_config_parameter set value = 'https://stage.compassion.se' where key = 'web.external.url';
+update ir_config_parameter set value = 'https://stage.compassion.se' where key = 'web.base.url';
+update ir_config_parameter set value = (
+    SELECT value FROM ir_config_parameter WHERE key = 'message_center_compassion.connect_api_key_stage'
+    ) where key = 'message_center_compassion.connect_api_key';
+update ir_config_parameter set value = (
+    SELECT value FROM ir_config_parameter WHERE key = 'message_center_compassion.delivery_service_api_key_stage'
+    ) where key = 'message_center_compassion.delivery_service_api_key';
+update ir_config_parameter set value = (
+    SELECT value FROM ir_config_parameter WHERE key = 'message_center_compassion.connect_client_stage'
+    ) where key = 'message_center_compassion.connect_client';
+update ir_config_parameter set value = (
+    SELECT value FROM ir_config_parameter WHERE key = 'message_center_compassion.connect_secret_stage'
+    ) where key = 'message_center_compassion.connect_secret';
+
+-- Admin password
+update res_users set password=(
+    SELECT value FROM ir_config_parameter WHERE key = 'compassion_nordic.admin_password_stage'
+    ) where login='admin';
+
+-- Deactivating CRON and Automated tasks
+update ir_cron set active = false;
+update base_automation SET active=false;
+
+-- Delete queue jobs
+delete from queue_job where state != 'done';
+
+-- Delete mailchimp account
+DELETE FROM mailchimp_template;
+DELETE FROM mailchimp_account;
