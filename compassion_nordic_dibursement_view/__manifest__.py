d10e8cb408af91aff5ca19f7a27a49f90a8f6335
NoeBerdoz <berdoznoe@gmail.com>
2025-09-23 15:00:34 +0200

    [MIG] compassion_nordic_dibursement_view: Migration to 17.0

--- compassion_nordic_dibursement_view/__manifest__.py
+++ compassion_nordic_dibursement_view/__manifest__.py
@@ -1,15 +1,16 @@
 {
     "name": "disbursement_view",
-    "summary": """
-        create a exportable view for the excel sheet of disbursement""",
+    "summary": "Create an exportable view for the excel sheet of disbursement",
     "author": "Compassion Switzerland",
     "license": "AGPL-3",
     "website": "https://github.com/CompassionCH/compassion-nordic",
     "category": "Uncategorized",
-    "version": "14.0.1.0.0",
-    "depends": ["base"],
+    "version": "17.0.1.0.0",
+    "depends": ["base", "account"],
     "data": [
         "security/ir.model.access.csv",
         "views/views.xml",
+        "security/ir.rule.xml"
     ],
+    "installable": True,
 }
--- compassion_nordic_dibursement_view/models/models.py
+++ compassion_nordic_dibursement_view/models/models.py
@@ -1,36 +1,42 @@
-from odoo import api, fields, models, tools
+from odoo import fields, models
+from odoo.tools import sql
 
 class DisbursementData(models.Model):
     _name = 'disbursement.data'
     _auto = False
     _description = 'Disbursement Data'
 
-    company = fields.Char(string='Company', required=True)
-    month = fields.Date(string='Month', required=True)
-    account = fields.Char(string='Account', required=True)
-    fund = fields.Char(string='Fund')
-    debit = fields.Float(string='Debit')
-    credit = fields.Float(string='Credit')
-    amount = fields.Float(string='Amount')
+    company = fields.Char(string='Company', required=True, readonly=True)
+    month = fields.Date(string='Month', required=True, readonly=True)
+    account = fields.Char(string='Account', required=True, readonly=True)
+    fund = fields.Char(string='Fund', readonly=True)
+    debit = fields.Float(string='Debit', readonly=True)
+    credit = fields.Float(string='Credit', readonly=True)
+    amount = fields.Float(string='Amount', readonly=True)
 
     def init(self):
-        tools.drop_view_if_exists(self.env.cr, 'disbursement_data')
+        sql.drop_view_if_exists(self.env.cr, self._table)
         self.env.cr.execute("""
-            CREATE OR REPLACE VIEW disbursement_data AS (
-            select row_number() over() as id, rc."name" as company, date_trunc('month', am."date")::date as month,
-                aa.code as account, pp.default_code as fund, sum(aml.debit) as debit,
-                sum(aml.credit) as credit, sum(aml.debit - aml.credit) as amount
-            from account_move_line aml
-            left join account_move am on am.id = aml.move_id
-            left join account_account aa on aa.id = aml.account_id
-            left join product_product pp on pp.id = aml.product_id
-            left join res_company rc on rc.id = am.company_id
-            where am.date > '2022-06-30' 
-                and aa.internal_type = 'other'
-                and am.state = 'posted'
-                and ((am.move_type = 'out_invoice' and am.payment_state = 'paid') or am.move_type <> 'out_invoice')
-                and (aa.code like '7%' or aa.code like '3%')
-            group by rc."name", date_trunc('month', am."date"), aa.code, pp.default_code
-            having (sum(aml.debit) > 0 or sum(aml.credit) > 0)
-            order by month)
-            """)
+            CREATE OR REPLACE VIEW %s AS (
+            SELECT row_number() over() as id,
+                rc."name" as company,
+                date_trunc('month', am."date")::date as month,
+                aa.code as account,
+                pp.default_code as fund,
+                sum(aml.debit) as debit,
+                sum(aml.credit) as credit,
+                sum(aml.debit - aml.credit) as amount
+            FROM account_move_line aml
+            LEFT JOIN account_move am on am.id = aml.move_id
+            LEFT JOIN account_account aa on aa.id = aml.account_id
+            LEFT JOIN product_product pp on pp.id = aml.product_id
+            LEFT JOIN res_company rc on rc.id = am.company_id
+            WHERE am.date > '2022-06-30'
+                AND aa.internal_type = 'other'
+                AND am.state = 'posted'
+                AND ((am.move_type = 'out_invoice' AND am.payment_state = 'paid') OR am.move_type <> 'out_invoice')
+                AND (aa.code LIKE '7%%' OR aa.code LIKE '3%%')
+            GROUP BY rc."name", date_trunc('month', am."date"), aa.code, pp.default_code
+            HAVING (sum(aml.debit) > 0 OR sum(aml.credit) > 0)
+            ORDER BY month)
+        """ % self._table)
