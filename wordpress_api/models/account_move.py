e956b5c7b2f27b215857e0591391eb576323562c
Emanuel Cino <ecino@compassion.ch>
2026-05-13 09:10:16 +0200

    T3174 FIX account search for WP donations

--- wordpress_api/models/account_move.py
+++ wordpress_api/models/account_move.py
@@ -42,7 +42,7 @@ class AccountMove(models.Model):
                     .search(
                         [
                             ("code", "=", "15100"),
-                            ("company_id", "=", self.company_id.id),
+                            ("company_ids", "=", self.company_id.id),
                         ],
                     )
                     .id,
